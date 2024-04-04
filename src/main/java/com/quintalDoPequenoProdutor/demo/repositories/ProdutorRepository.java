330afcd552d0edd132978bd3724c7b5ea968ed6b
Karen Boscolo <karenboscolo@yahoo.com.br>
2024-04-03 21:31:10 -0300

    Criaçao entities

---
+++ src/main/java/com/quintalDoPequenoProdutor/demo/repositories/ProdutorRepository.java
@@ -0,0 +1,8 @@
+package com.quintalDoPequenoProdutor.demo.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutorRepository extends JpaRepository {
+}
