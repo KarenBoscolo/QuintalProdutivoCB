68582fd52f025a9e98ccb8026a025047f728d865
Karen Boscolo <karenboscolo@yahoo.com.br>
2024-04-03 21:22:30 -0300

    Criaçao entities

---
+++ src/test/java/com/quintalDoPequenoProdutor/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quintalDoPequenoProdutor.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
